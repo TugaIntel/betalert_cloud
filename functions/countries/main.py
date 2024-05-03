aa95af6112c70542fce13b5556a5fbd25130f1ce
TugaIntel <Tugaintelligence@gmail.com>
2024-05-03 11:04:30 +0200

    v0.1 countries

--- functions/countries/main.py
+++ functions/countries/main.py
@@ -1,36 +1,71 @@
 #!/usr/bin/env python3
 import time
+import logging
+import google.cloud.logging
 from utils import get_db_connection, make_api_call  # Import utility functions
-from log import setup_logger  # Import logging functionality
 from config_loader import load_config  # Import configuration loader
 
-# Set up logger for the countries entity
-logger = setup_logger('countries', 'INFO')
+# Sets up Google Cloud logging with the default client.
+client = google.cloud.logging.Client()
+client.setup_logging()
+
+# Load configuration settings
 config = load_config()
 
 
 def get_existing_countries(cursor):
+    """
+    Fetches existing countries from the database.
+
+    Args:
+        cursor (Cursor): A database cursor to execute database operations.
+
+    Returns:
+        dict: A dictionary of countries with country IDs as keys and country names as values.
+    """
     cursor.execute("SELECT id, name FROM countries")
     return {row[0]: {"name": row[1]} for row in cursor.fetchall()}
 
 
 def insert_country(cursor, conn, country_id, country_name):
+    """
+    Inserts a new country into the database.
 
+    Args:
+        cursor (Cursor): A database cursor to execute database operations.
+        conn (Connection): A database connection object to commit changes.
+        country_id (int): The ID of the country to insert.
+        country_name (str): The name of the country to insert.
+    """
     insert_country_sql = "INSERT INTO countries (id, name) VALUES (%s, %s)"
     cursor.execute(insert_country_sql, (country_id, country_name))
     conn.commit()
 
 
 def update_country(cursor, conn, country_id, country_name):
+    """
+    Updates an existing country's name in the database.
 
+    Args:
+        cursor (Cursor): A database cursor to execute database operations.
+        conn (Connection): A database connection object to commit changes.
+        country_id (int): The ID of the country to update.
+        country_name (str): The new name for the country.
+    """
     update_country_sql = "UPDATE countries SET name = %s WHERE id = %s"
     cursor.execute(update_country_sql, (country_name, country_id))
     conn.commit()
 
 
 def fetch_countries():
+    """
+    Fetches country data from the API.
 
+    Returns:
+        list of dict: Parsed JSON response data from the API, or None if the API call fails.
+    """
     endpoint = config['api']['endpoints']['countries']  # Access endpoint from config
+    logging.debug(f"Fetching countries from API endpoint: {endpoint}")
     response_data = make_api_call(endpoint)
 
     if response_data:
@@ -40,23 +75,42 @@ def fetch_countries():
 
 
 def parse_countries_data(json_data):
+    """
+    Parses JSON data containing countries information.
 
+    Args:
+        json_data (dict): The JSON response data from the API call.
+
+    Returns:
+        list: A list of dictionaries, each containing a country's ID and name.
+    """
     countries = []
     if json_data and "categories" in json_data:
         for category in json_data["categories"]:
             countries.append({"id": category["id"], "name": category["name"]})
+    logging.debug(f"Parsed {len(countries)} countries from API data.")
     return countries
 
 
 def countries_main(request):
+    """
+    Main function to handle country data fetching and updating.
 
+    Args:
+        request: The request payload for HTTP Cloud Function.
+
+    Returns:
+        tuple: Response message and HTTP status code.
+    """
     start_time = time.time()
+
+    logging.info("Countries function execution started.")
+
     engine = get_db_connection()  # This is an SQLAlchemy engine now
+    conn = engine.raw_connection()  # Gets a raw connection from the engine
+    cursor = conn.cursor()
 
     try:
-        conn = engine.raw_connection()  # Gets a raw connection from the engine
-        cursor = conn.cursor()
-
         # Initialize counters for inserted and updated countries
         inserted_count = 0
         updated_count = 0
@@ -84,13 +138,15 @@ def countries_main(request):
                     update_country(cursor, conn, country_id, country['name'])
                     updated_count += 1
 
+        logging.info(f"Inserted {inserted_count} new countries and updated {updated_count} existing countries.")
+
     except Exception as e:
-        logger.error(f"An error occurred: {e}")
+        logging.error(f"An error occurred: {e}")
+        return f'An error occurred: {str(e)}', 500
 
     finally:
         cursor.close()  # Ensure the cursor is closed after operations
         conn.close()  # Ensure the connection is closed after operations
 
-    logger.info(f"Total execution time: {time.time() - start_time:.4f} seconds")
-
-
+    logging.info(f"Total execution time: {time.time() - start_time:.4f} seconds")
+    return 'Function executed successfully', 200
--- functions/countries/utils.py
+++ functions/countries/utils.py
@@ -3,11 +3,17 @@ import json
 import requests
 import pymysql
 import sqlalchemy
-from config_loader import load_config
+import logging
+import google.cloud.logging
 from google.cloud import secretmanager
 from google.cloud.sql.connector import Connector, IPTypes
+from config_loader import load_config  # Import configuration loader
 
-# Setup logger for API calls
+# Sets up Google Cloud logging with the default client.
+client = google.cloud.logging.Client()
+client.setup_logging()
+
+# Load configuration settings
 config = load_config()
 
 
@@ -20,7 +26,7 @@ def get_db_connection() -> sqlalchemy.engine.base.Engine:
     """
     instance_connection_name = os.getenv("INSTANCE_CONNECTION_NAME")
     db_user = os.getenv("DB_USER")
-    db_pass = os.getenv("DB_PASS")
+    db_pass = load_secret_version('DB_PASS')
     db_name = os.getenv("DB_NAME")
     use_private_ip = os.getenv("PRIVATE_IP", "false").lower() == "true"
 
@@ -29,6 +35,10 @@ def get_db_connection() -> sqlalchemy.engine.base.Engine:
     connector = Connector(ip_type=ip_type)
 
     def getconn() -> pymysql.connections.Connection:
+        """
+        Returns a new connection from the pool.
+        Used internally by SQLAlchemy's create_engine.
+        """
         return connector.connect(
             instance_connection_name,
             "pymysql",
@@ -41,13 +51,24 @@ def get_db_connection() -> sqlalchemy.engine.base.Engine:
         "mysql+pymysql://",
         creator=getconn,
     )
-
+    logging.info("SQLAlchemy connection pool created.")
     return pool
 
 
 def make_api_call(endpoint, params=None):
+    """
+    Performs an API call to a specified endpoint.
+
+    Args:
+        endpoint (str): The API endpoint to call.
+        params (dict, optional): Parameters to pass to the API call.
+
+    Returns:
+        dict or None: The JSON response from the API call or None if an error occurs.
+    """
     full_url = config['api']['base_url'] + endpoint
     headers = config['headers']
+    logging.debug(f"Making API call to {full_url}.")
 
     try:
         response = requests.get(full_url, headers=headers, params=params)
@@ -55,22 +76,41 @@ def make_api_call(endpoint, params=None):
             return response.json()
         else:
             return None
-    except requests.RequestException:
+    except requests.RequestException as e:
+        logging.error(f"API call failed: {e}")
         return None
 
 
 def load_secret_version(secret_id):
-    """Load a secret version from Google Cloud Secret Manager."""
+    """
+        Retrieves a secret from Google Cloud Secret Manager.
+
+        Args:
+            secret_id (str): The ID of the secret to retrieve.
+
+        Returns:
+            str: The secret value.
+    """
     client = secretmanager.SecretManagerServiceClient()
-    project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+    project_id = os.getenv('PROJECT_ID')
     name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
     response = client.access_secret_version(request={"name": name})
     return response.payload.data.decode('UTF-8')
 
 
 def send_alert(message):
+    """
+    Sends an alert message to specified Telegram chat IDs using a bot.
+
+    Args:
+        message (str): The message to send.
+    """
     bot_token = load_secret_version('telegram-bot-token')
     chat_ids = json.loads(load_secret_version('telegram-chat-ids'))
     send_url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     for chat_id in chat_ids:
-        requests.post(send_url, data={'chat_id': chat_id, 'text': message})
+        try:
+            response = requests.post(send_url, data={'chat_id': chat_id, 'text': message})
+            response.raise_for_status()
+        except requests.RequestException as e:
+            logging.error(f"Failed to send message to chat ID {chat_id}: {e}")
