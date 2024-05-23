beb40978064594bd08c8671a93aa646061aaa3cc
TugaIntel <Tugaintelligence@gmail.com>
2024-05-23 18:45:33 +0200

    v1.5 - New DJango

--- betalert/matches/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import home
-
-urlpatterns = [
-    path('', home, name='home'),  # Default to today
-    path('matches/<str:day>/', home, name='matches_by_day'),
-]
